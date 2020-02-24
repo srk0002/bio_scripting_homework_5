5ce4976fc0ec3068b86319050788da66b0be5a24
Steven R. Kitchens <srk0002@auburn.edu>
2020-02-23 19:05:51 -0600

    added git directory

---
+++ part_1/fix_me.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Reading data from the user's input
+echo ' '
+echo 'Enter a number for the following values of a and b'
+echo ' '
+
+echo 'Enter a : '
+read a
+echo 'Enter b : ' 
+read b
+
+add=$((a + b))
+
+echo Addition of a and b are $add
+
+sub=$((a - b))
+echo Subtraction of a and b are $sub
+
+mul=$((a * b))
+echo Multiplication of a and b are $mul
+
+div=$((a / b))
+echo Division of a and b are $div
+
+mod=$((a % b))
+echo Modulus of a and b are $mod
+
+((++a))
+echo Increment operator when applied on "a" results into a = $a
+
+((--b))
+echo Decrement operator when applied on "b" results into b = $b
+
+((--c))
+echo What was the default value \ of c if its value is now $c\?
+echo -1
